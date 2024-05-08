a4390b1e3d54211fd1161dce700cc2830b8e860a
hango1705 <haninhquang2003@gmail.com>
2024-05-09 02:54:52 +0700

    3rd commit

--- upload.php
+++ upload.php
@@ -1,5 +1,4 @@
 <?php
-// Kết nối với cơ sở dữ liệu
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
@@ -11,18 +10,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Kiểm tra xem có tập tin đã được tải lên không
 if ($_FILES["videoFile"]["error"] == UPLOAD_ERR_OK) {
     $videoName = $_FILES["videoFile"]["name"];
     $videoTmpName = $_FILES["videoFile"]["tmp_name"];
     $videoType = $_FILES["videoFile"]["type"];
 
-    // Di chuyển tập tin tải lên vào thư mục mong muốn
     $targetDirectory = "D:/uploads/";
     $targetFile = $targetDirectory . basename($videoName);
 
     if (move_uploaded_file($videoTmpName, $targetFile)) {
-        // Lưu đường dẫn của video vào cơ sở dữ liệu
         $videoPath = $targetFile;
         $sql = "INSERT INTO videos (video_path) VALUES ('$videoPath')";
 
